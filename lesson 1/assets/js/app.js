db16120f8e8b7969067dab550885ad0bcfc79540
dende56 <117033993+dende56@users.noreply.github.com>
2023-06-21 18:14:05 +0300

    Add files via upload

---
+++ lesson 1/assets/js/app.js
@@ -0,0 +1,49 @@
+// let number = 83;
+const pi = 3.14;
+
+// let title = "Hello world!";
+
+// title = title + " The weather is good.";
+
+// console.log(title);
+
+// number = number - 3;
+
+// console.log(number);
+
+let word = "2";
+
+// // console.log(word * permanent2);
+// console.log(permanent2 === word);
+
+const object = {
+  name: "Name",
+  joyOff1: "joy on",
+  joyOn: "joy off",
+};
+
+// console.log(object);
+// console.log(object['joy'+ 'On']);
+// console.log(object['joy'+'Off']);
+
+let temp = null;
+
+// console.log(typeof pi);
+
+const array = [1,2,3,'word1','word2','word3'];
+
+// console.log(array)
+// console.log(array[0])
+// console.log(array[3])
+// console.log(array[5])
+
+//types
+// *********
+//
+// number
+// string
+// NaN
+// bool < > == >= <= ===
+// object
+// undefined
+// null
